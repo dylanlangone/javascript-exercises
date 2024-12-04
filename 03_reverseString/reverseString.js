89a4411128897ab9911367a4fb1524a9a0b805ff
Dylan Langone <dylanlangone628@gmail.com>
2024-12-04 17:17:27 -0500

    Complete 02_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    result = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string.charAt(i);
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
