439c98a8ce83fe46852236942b1abcb817a8f00b
Dylan Langone <dylanlangone628@gmail.com>
2024-12-05 17:44:13 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
+// removes arguments from array
 const removeFromArray = function() {
+    result = []
+    arr = arguments[0];
+    for (let i = 0; i < arr.length; i++) {
+        let skip = false;
+        for (let j = 1; j < arguments.length; j++) {
+            if (arr[i] === arguments[j]) {
+                skip = true;
+            }
+        }
+        if (skip) {
+            continue;
+        }
+        result.push(arr[i]);
+    }
+    // console.log("result: " + result)
+    return result;
 };
 
 // Do not edit below this line
