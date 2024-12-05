1d6f3cfd3b506b74c8623fa5cfd5df14f84d12d5
Dylan Langone <dylanlangone628@gmail.com>
2024-12-05 18:02:26 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let result = 0;
 
+    if (!Number.isInteger(start) || !Number.isInteger(stop)) {
+        return 'ERROR';
+    }
+
+    if (typeof(stop) === 'string') {
+        return 'ERROR'
+    }
+
+    if (start < 0) {
+        return 'ERROR';
+    }
+
+    // swap if start > stop
+    if (start > stop) {
+        let temp = stop;
+        stop = start;
+        start = temp;
+    }
+    for (let i = start; i <= stop; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
